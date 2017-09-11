85079a0549f537d5c67524b22c16bf93d1abe32b
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-09-11 12:04:29 +0200

    Fix broken path

--- src/extension.js
+++ src/extension.js
@@ -5,7 +5,7 @@ var path = require('path')
 
 function activate(context) {
     var cmd = vscode.commands.registerCommand('extension.openFileFolder', function (e) {
-        let folderName = path.dirname(e._path);
+        let folderName = path.dirname(e.path);
         let folderUrl = vscode.Uri.file(folderName)
         vscode.commands.executeCommand("vscode.openFolder", folderUrl, false)
     });
